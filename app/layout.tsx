29b706d6f720ded878ef7b85556a6ff7b66f52e5
Shreshta Rajput <shreshtarajput1802@gmail.com>
2026-04-16 13:04:03 +0530

    fixing rendering issuesF

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${outfit.variable} ${dmSans.variable} h-full antialiased`}
+      className={`dark ${geistSans.variable} ${geistMono.variable} ${outfit.variable} ${dmSans.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col font-sans">
         <AuthProvider>
